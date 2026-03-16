916449963ce390c159390e593e0b8aac7a7abbc4
Adnane BC <adnane.bachchar@gmail.com>
2026-03-16 02:00:59 +0000

    replace bcrypt with bcryptjs for deployment

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { IsString, IsEmail, IsOptional, IsEnum } from 'class-validator';
 import { BusinessRole, OrganizationRole } from '@prisma/client';
 
